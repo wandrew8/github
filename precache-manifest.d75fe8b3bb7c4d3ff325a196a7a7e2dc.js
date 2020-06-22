803443a73e23dc5422b8fc5c6d4f1c9477468673
Andrew Weiss <wandrew8@gmail.com>
2020-06-22 09:53:55 -0700

    Updates

---
+++ precache-manifest.d75fe8b3bb7c4d3ff325a196a7a7e2dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2543e2ddb23ed9533f885cb83eaf014",
+    "url": "/github/index.html"
+  },
+  {
+    "revision": "0ca44f6d91e346d8c3c6",
+    "url": "/github/static/css/main.eb181995.chunk.css"
+  },
+  {
+    "revision": "dfa1925e9e8a1ce57ae6",
+    "url": "/github/static/js/2.1b91cd37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github/static/js/2.1b91cd37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ca44f6d91e346d8c3c6",
+    "url": "/github/static/js/main.0e7d50a4.chunk.js"
+  },
+  {
+    "revision": "003380dff38504b52e7b",
+    "url": "/github/static/js/runtime-main.bd1faca5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github/static/media/logo.5d5d9eef.svg"
+  }
+]);
